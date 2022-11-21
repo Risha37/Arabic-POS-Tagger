6a97c82606d831ce2a6b022988e139661f00e29b
unknown <belalmoatla2002@gmail.com>
2022-11-21 17:34:38 +0300

    update 1.5

--- tests/Accuracy.py
+++ tests/Accuracy.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 name = ''
 
-corpus = pd.read_csv("{}.csv".format(name)).iloc[:, -1]
-test = pd.read_csv("{} result.csv".format(name)).iloc[:, -1]
+corpus = pd.read_csv("{}.csv".format(name)).iloc[1:, -1]
+test = pd.read_csv("{} result.csv".format(name)).iloc[1:, -1]
 
-count =0
+count = 0
 
 for i in range(len(corpus)):
     if corpus[i] == test[i]:
